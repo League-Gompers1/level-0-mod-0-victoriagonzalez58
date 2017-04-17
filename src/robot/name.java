f7bb76c3ca8ccaa5a2c36f5507caab2b8e91d04b
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2017-04-17 14:52:24 -0700

    Add files via upload

---
+++ src/robot/name.java
@@ -0,0 +1,20 @@
+package robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class name {
+public static void main(String[] args) {
+	Robot bill = new Robot();
+	bill.turn(270);
+			bill.move(400);
+			bill.setSpeed(10);
+			bill.turn(120);
+			bill.penDown();
+			bill.turn(120);
+			bill.move(100);
+			bill.turn(90);
+			
+	
+}
+}
+
